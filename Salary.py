d2d5395589f35dad80174bb1b27236e3880cbce9
leontiz <leontis.qa@gmail.com>
2019-04-26 23:24:46 +0300

    Update Salary App from home #2

--- Salary.py
+++ Salary.py
@@ -6,16 +6,16 @@ class App(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        string_label = tk.Label(master, text="File name: ")
-        address_string = tk.Entry(master, width=50)
-        open_button = tk.Button(master, text="Browse...", command=lambda: self.open_file())
-        string_label.pack(side="left")
-        address_string.pack(side="left")
-        open_button.pack(side="right")
+        self.string_label = tk.Label(master, text="File name: ")
+        self.address_string = tk.Entry(master, width=50)
+        self.open_button = tk.Button(master, text="Browse...", command=lambda: self.open_file())
+        self.string_label.pack(side="left")
+        self.address_string.pack(side="left")
+        self.open_button.pack(side="right")
 
     def open_file(self):
         filename = filedialog.askopenfilename()
-        address_string.insert(0, filename)
+        self.address_string.insert(0, filename)
 
 window = tk.Tk()
 app = App(window)
